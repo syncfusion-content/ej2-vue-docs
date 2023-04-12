69f909cb70ceff2e5bce8de3823b3d3da70d8090
Build Automaion <buildautomation@syncfusion.com>
2023-04-12 14:52:21 +0530

    Integrated latest changes at 04-12-2023 1:15:30 PM

--- ej2-vue/code-snippet/kanban/ajax-cs1/app.vue
+++ ej2-vue/code-snippet/kanban/ajax-cs1/app.vue
@@ -1,17 +1,15 @@
-
-
 <template>
   <div id="app">
-    <ejs-button v-on:click.native="btnClick">Load Data</ejs-button>
-       <ejs-kanban ref="kanbanObj" id="kanban" keyField="ShipCountry" :dataSource="kanbanData"
-        :cardSettings="cardSettings">
-          <e-columns>
-            <e-column headerText="Denmark" keyField="Denmark"></e-column>
-            <e-column headerText="Brazil" keyField="Brazil"></e-column>
-            <e-column headerText="Switzerland" keyField="Switzerland"></e-column>
-            <e-column headerText="Germany" keyField="Germany"></e-column>
-          </e-columns>
-        </ejs-kanban>
+<ejs-button v-on:click.native="btnClick">Load Data</ejs-button>
+   <ejs-kanban ref="kanbanObj" id="kanban" keyField="ShipCountry"
+    :cardSettings="cardSettings">
+      <e-columns>
+        <e-column headerText="Denmark" keyField="Denmark"></e-column>
+        <e-column headerText="Brazil" keyField="Brazil"></e-column>
+        <e-column headerText="Switzerland" keyField="Switzerland"></e-column>
+        <e-column headerText="Germany" keyField="Germany"></e-column>
+      </e-columns>
+    </ejs-kanban>
   </div>
 </template>
 
@@ -34,16 +32,17 @@ export default {
   },
   methods: {
     btnClick: function (args){
-        this.kanbanObj = this.$refs.kanbanObj.ej2Instances;
+        var kanbanData = this.$refs.kanbanObj.ej2Instances;
         var ajax = new Ajax("https://ej2services.syncfusion.com/production/web-services/api/Orders", "GET");
         ajax.send();
         ajax.onSuccess = function (result) {
-            this.kanbanObj.dataSource = JSON.parse(result);
+            kanbanData.dataSource = JSON.parse(result);
         };
-      }
+    }
   }
 }
 </script>
+
 <style>
 @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
 @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
@@ -54,6 +53,3 @@ export default {
 @import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
 @import '../node_modules/@syncfusion/ej2-vue-kanban/styles/material.css';
 </style>
-
-
-
