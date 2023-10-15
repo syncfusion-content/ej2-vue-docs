17269c24493b227d85705f296c4a56dd0efe966f
Build Automaion <buildautomation@syncfusion.com>
2023-10-15 07:50:50 +0530

    Integrated latest changes at 10-15-2023 7:30:11 AM

--- ej2-vue/code-snippet/pdfviewer/addformfield-cs1/app.vue
+++ ej2-vue/code-snippet/pdfviewer/addformfield-cs1/app.vue
@@ -5,7 +5,8 @@
             id="pdfViewer"
             ref="pdfviewer"
             :documentPath="documentPath"
-            :documentLoad="documentLoad">
+            :documentLoad="documentLoad"
+            :resourceUrl="resourceUrl">
         </ejs-pdfviewer>
     </div>
 </template>
@@ -21,7 +22,8 @@ export default {
   name: 'app',
   data () {
     return {
-      documentPath:"https://cdn.syncfusion.com/content/pdf/form-designer.pdf"
+      documentPath:"https://cdn.syncfusion.com/content/pdf/form-designer.pdf",
+      resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib"
     };
   },
 
