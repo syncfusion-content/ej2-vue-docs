4035d835762329aa7fb235df045bbd01e2d64e3a
Build Automaion <buildautomation@syncfusion.com>
2023-10-17 08:45:52 +0530

    Integrated latest changes at 10-17-2023 7:30:24 AM

--- ej2-vue/code-snippet/list-box/getting-started/getting-started-cs21/index.js
+++ ej2-vue/code-snippet/list-box/getting-started/getting-started-cs21/index.js
@@ -10,7 +10,7 @@ new Vue({
 	template: `
   <div id="app">
     <div id='container' style="margin:10px auto 0; width:250px;">
-        <ejs-listbox :dataSource='data' :cssClass='e-horizontal-listbox' ></ejs-listbox>
+        <ejs-listbox :dataSource='data' cssClass='e-horizontal-listbox' ></ejs-listbox>
     </div>
   </div>
 `,
