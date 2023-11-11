5cb5a1d5576a73af6987b057b866834342a84afc
Build Automaion <buildautomation@syncfusion.com>
2023-11-11 07:57:18 +0530

    Integrated latest changes at 11-11-2023 7:30:13 AM

--- ej2-vue/code-snippet/diagram/getting-started/connectnode-cs1/index.js
+++ ej2-vue/code-snippet/diagram/getting-started/connectnode-cs1/index.js
@@ -4,26 +4,25 @@
     Vue.use(DiagramPlugin);
     let nodes = [
         {
-            id: "node1",
-            height: 100,
-            width: 100,
-            offsetX: 200,
-            offsetY: 100,
+            id: 'Start', width: 140, height: 50, offsetX: 300, offsetY: 50,
+            annotations: [{
+                id: 'label1',
+                content: 'Start'
+            }],
+            shape: { type: 'Flow', shape: 'Terminator'}
         },
         {
-            id: "node2",
-            height: 100,
-            width: 100,
-            offsetX: 200,
-            offsetY: 300,
+            id: 'Init', width: 140, height: 50, offsetX: 300, offsetY: 140,
+            shape: { type: 'Flow', shape: 'Process' },
+            annotations: [{ content: 'var i = 0;' }]
         }
     ];
     let connectors = [
         {
             id: "connector1",
-            sourceID: "node1",
-            targetID: "node2"
-        },
+            sourceID: "Start",
+            targetID: "Init"
+        }
     ]
     
 new Vue({
@@ -34,7 +33,7 @@ new Vue({
     </div>
 `,
 
-    name: 'app'
+    name: 'app',
         data () {
             return {
                 width: "100%",
--- ej2-vue/pdfviewer/getting-started.md
+++ ej2-vue/pdfviewer/getting-started.md
@@ -144,7 +144,37 @@ Output will be displayed as follows.
 
 {% tabs %}
 {% highlight html tabtitle="app.vue" %}
-{% include code-snippet/pdfviewer/getting-started-cs2/app.vue %}
+<template>
+  <div id="app">
+    <ejs-pdfviewer 
+      id="pdfViewer" 
+      :documentPath="documentPath"
+      :resourceUrl="resourceUrl"> 
+    </ejs-pdfviewer>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { PdfViewerPlugin, Toolbar, Magnification, Navigation, LinkAnnotation, 
+         BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, 
+         Annotation, FormDesigner, FormFields } from '@syncfusion/ej2-vue-pdfviewer';
+Vue.use(PdfViewerPlugin);
+export default {
+  name: 'app',
+  data () {
+    return {
+      documentPath:"https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf",
+      resourceUrl:"https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib",
+    };
+  },
+
+  provide: {
+    PdfViewer: [ Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView,
+                 Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner ]
+  }
+}
+</script>
 {% endhighlight %}
 {% endtabs %}
         
