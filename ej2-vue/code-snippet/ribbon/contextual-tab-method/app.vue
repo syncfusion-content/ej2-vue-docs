bd4bf71cd8495fe39a3d14c2b68cadf0504d9bdb
Build Automaion <buildautomation@syncfusion.com>
2024-03-21 04:43:58 +0530

    Integrated latest changes at 03-21-2024 4:30:06 AM

---
+++ ej2-vue/code-snippet/ribbon/contextual-tab-method/app.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <button class="e-btn" id="show-contextual" @click="showContextualTab"> Show tab </button>
+    <button class="e-btn" id="hide-contextual" @click="hideContextualTab"> Hide tab </button>
+    <ejs-ribbon ref="ribbonContextualTab">
+      <e-ribbon-tabs>
+        <e-ribbon-tab header="Home">
+          <e-ribbon-groups>
+            <e-ribbon-group header="Clipboard" showLauncherIcon=true groupIconCss="e-icons e-paste" id="clipboardGroup">
+              <e-ribbon-collections>
+                <e-ribbon-collection>
+                  <e-ribbon-items>
+                    <e-ribbon-item type="Button" :buttonSettings="cutButton">
+                    </e-ribbon-item>
+                    <e-ribbon-item type="Button" :buttonSettings="copyButton">
+                    </e-ribbon-item>
+                    <e-ribbon-item type="Button" :buttonSettings="formatButton">
+                    </e-ribbon-item>
+                  </e-ribbon-items>
+                </e-ribbon-collection>
+              </e-ribbon-collections>
+            </e-ribbon-group>
+          </e-ribbon-groups>
+        </e-ribbon-tab>
+      </e-ribbon-tabs>
+      <e-ribbon-contextual-tabs>
+        <e-ribbon-contextual-tab>
+          <e-ribbon-tabs>
+            <e-ribbon-tab header="Arrange & View" id="ArrangeView">
+              <e-ribbon-groups>
+                <e-ribbon-group header="Arrange" showLauncherIcon=true>
+                <e-ribbon-collections>
+                  <e-ribbon-collection>
+                    <e-ribbon-items>
+                      <e-ribbon-item type="Button" :buttonSettings="forward">
+                      </e-ribbon-item>
+                      <e-ribbon-item type="Button" :buttonSettings="backward">
+                      </e-ribbon-item>
+                      <e-ribbon-item type="Button" :buttonSettings="selection ">
+                      </e-ribbon-item>
+                    </e-ribbon-items>
+                  </e-ribbon-collection>
+                </e-ribbon-collections>
+                </e-ribbon-group>
+              </e-ribbon-groups>
+            </e-ribbon-tab>
+          </e-ribbon-tabs>
+        </e-ribbon-contextual-tab>
+      </e-ribbon-contextual-tabs>
+    </ejs-ribbon>
+  </div>
+</template>
+
+<script>
+  import Vue from "vue";
+  import { RibbonItemSize, RibbonPlugin, RibbonContextualTab } from "@syncfusion/ej2-vue-ribbon";
+  Vue.use(RibbonPlugin);
+
+  export default {
+    provide: {
+      ribbon: [RibbonContextualTab]
+    },
+    data: function () {
+      return {
+        largeSize: RibbonItemSize.Large,
+        cutButton:  { iconCss: "e-icons e-cut", content: "Cut" },
+        copyButton:  { iconCss: "e-icons e-copy", content: "Copy" },
+        formatButton:  { iconCss: "e-icons e-format-painter", content: "Format Painter" },
+        forward:  { iconCss: "e-icons e-bring-forward", content: "Bring Forward" },
+        backward:  { iconCss: "e-icons e-send-backward", content: "Send Backward" },
+        selection:  { iconCss: "e-icons e-show-hide-panel", content: "Selection Pane" }
+      };
+    },
+    methods: {
+      showContextualTab() {
+        this.$refs.ribbonContextualTab.showTab('ArrangeView', true);
+      },
+      hideContextualTab() {
+        this.$refs.ribbonContextualTab.hideTab('ArrangeView', true);
+      }
+    }
+  };
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";  
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-lists/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-ribbon/styles/material.css";
+</style>
---
+++ ej2-vue/file-manager/how-to/perform-custom-sorting.md
@@ -0,0 +1,25 @@
+---
+layout: post
+title: Perform custom sorting in Vue FileManager component | Syncfusion
+description: Learn here all about How to perform custom sorting in Syncfusion Vue FileManager component of Syncfusion Essential JS 2 and more.
+control: Perform custom sorting 
+platform: ej2-vue
+documentation: ug
+domainurl: ##DomainURL##
+---
+
+# Perform custom sorting in Vue FileManager component
+
+The FileManager component provides a way to customize the default sort action for the LargeIconsView by defining the [sortComparer](https://ej2.syncfusion.com/vue/documentation/api/file-manager#sortcomparer) property and for sorting individual columns in the DetailsView by defining the [sortComparer](https://ej2.syncfusion.com/vue/documentation/api/file-manager/column/#sortcomparer) property in the [columns](https://ej2.syncfusion.com/vue/documentation/api/file-manager/column/) property.
+
+>Note: To achieve natural sorting like Windows Explorer, you can import the `SortComparer` function from the `'@syncfusion/ej2-vue-filemanager'`. If you want to perform your own custom sorting, you can define your own `SortComparer` function.
+
+The following example demonstrates how to define custom sort comparer function to achieve natural sorting behavior for the LargeIconsView and name column in DetailsView.
+
+{% tabs %}
+{% highlight html tabtitle="app.vue" %}
+{% include code-snippet/file-manager/customsorting-cs1/app.vue %}
+{% endhighlight %}
+{% endtabs %}
+        
+{% previewsample "page.domainurl/code-snippet/file-manager/customsorting-cs1" %}
