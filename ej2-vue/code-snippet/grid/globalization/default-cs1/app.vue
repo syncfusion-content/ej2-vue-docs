74b346dde3c6c77e54b6de715b742064e9a8e18c
Build Automaion <buildautomation@syncfusion.com>
2024-05-10 08:19:10 +0530

    Integrated latest changes at 05-10-2024 7:31:02 AM

---
+++ ej2-vue/code-snippet/grid/globalization/default-cs1/app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <ejs-grid :dataSource='data' locale='de-DE' :allowGrouping='true' :allowPaging='true' :pageSettings='pageOptions' height='220px'>
+      <e-columns>
+        <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=90></e-column>
+        <e-column field='CustomerID' headerText='Customer ID' width=100></e-column>
+        <e-column field='ShipCity' headerText='Ship City' width=100></e-column>
+        <e-column field='ShipName' headerText='Ship Name' width=120></e-column>
+      </e-columns>
+    </ejs-grid>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { L10n } from '@syncfusion/ej2-base';
+import { GridPlugin, Page, Group } from "@syncfusion/ej2-vue-grids";
+import deDELocalization from './locale.json';
+import { data } from './datasource.js';
+
+L10n.load(deDELocalization);
+Vue.use(GridPlugin);
+
+export default {
+  data() {
+    return {
+      data: data,
+      pageOptions: { pageSize: 7 }
+    };
+  },
+  provide: {
+    grid: [Page, Group]
+  }
+}
+</script>
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-calendars/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-vue-grids/styles/tailwind.css";
+</style>
+
+
+
