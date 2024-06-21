cd2e58de7fd99a7142fc187bb92fb912f5b1a023
Build Automaion <buildautomation@syncfusion.com>
2024-06-22 04:59:45 +0530

    Integrated latest changes at 06-22-2024 4:30:16 AM

--- ej2-vue/code-snippet/query-builder/drag-and-drop-cs1/index.js
+++ ej2-vue/code-snippet/query-builder/drag-and-drop-cs1/index.js
@@ -8,7 +8,7 @@ new Vue({
 	template: `
     <div class="control-section">
         <div class="col-lg-12 querybuilder-control">
-            <ejs-querybuilder ref="querybuilder" width="100%" :dataSource="dataSource" :rule="importRules" allowDragAndDrop="true">
+            <ejs-querybuilder ref="querybuilder" width="100%" :dataSource="dataSource" :rule="importRules" :allowDragAndDrop="true">
                 <e-columns>
                     <e-column field='EmployeeID' label='Employee ID' type='number' />
                     <e-column field='FirstName' label='First Name' type='string' />
@@ -35,24 +35,21 @@ new Vue({
                     field: "FirstName", 
                     type: "string", 
                     operator: "startswith", 
-                    value: "Andre", 
-                    condition: "and" 
+                    value: "Andre"
                 },
                 { 
                     label: "Last Name", 
                     field: "LastName", 
                     type: "string", 
                     operator: "in", 
-                    value: ['Davolio', 'Buchanan'], 
-                    condition: "or" 
+                    value: ['Davolio', 'Buchanan'] 
                 },
                 { 
                     label: "Age", 
                     field: "Age", 
                     type: "number", 
                     operator: "greaterthan", 
-                    value: 29, 
-                    condition: "and" 
+                    value: 29
                 },
                 {
                     condition: "or", rules: [
@@ -60,8 +57,7 @@ new Vue({
                         field: "IsDeveloper", 
                         type: "boolean", 
                         operator: "equal", 
-                        value: true, 
-                        condition: "and" 
+                        value: true
                     },
                     { 
                     label: "Primary Framework", 
