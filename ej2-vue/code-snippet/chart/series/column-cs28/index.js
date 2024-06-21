59018e767b518048bdcdf0c3e98db03e161542b3
Build Automaion <buildautomation@syncfusion.com>
2024-06-21 07:45:55 +0530

    Integrated latest changes at 06-21-2024 7:30:07 AM

---
+++ ej2-vue/code-snippet/chart/series/column-cs28/index.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import { ChartPlugin, ColumnSeries, Category } from "@syncfusion/ej2-vue-charts";
+import { DataManager, Query, ODataV4Adaptor } from '@syncfusion/ej2-data';
+
+Vue.use(ChartPlugin);
+
+let dataManager = new DataManager({
+  url: 'https://services.odata.org/V4/Northwind/Northwind.svc/Orders',
+  adaptor: new ODataV4Adaptor()
+});
+let query = new Query();
+
+;
+new Vue({
+	el: '#app',
+	template: `
+    <div id="app">
+        <ejs-chart id="container" :primaryXAxis='primaryXAxis' :title='title'>
+            <e-series-collection>
+                <e-series :dataSource='seriesData' type='Column' xName='CustomerID' yName='Freight' :query='queries'>
+                </e-series>
+            </e-series-collection>
+        </ejs-chart>
+    </div>
+`,
+
+  data() {
+    return {
+      seriesData: dataManager,
+      queries: query,
+      primaryXAxis: {
+        valueType: 'Category'
+      },
+      title: 'Order Details'
+    };
+  },
+  provide: {
+    chart: [ColumnSeries, Category]
+  }
+
+});
