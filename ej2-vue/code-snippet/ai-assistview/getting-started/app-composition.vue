bffe299fd037564033a34efc45ea931cf9af7480
KirupaSF4438 <161321983+KirupaSF4438@users.noreply.github.com>
2024-09-25 11:59:38 +0530

    911015: updated getting started contents and samples

--- ej2-vue/code-snippet/ai-assistview/getting-started/app-composition.vue
+++ ej2-vue/code-snippet/ai-assistview/getting-started/app-composition.vue
@@ -13,5 +13,6 @@ import { AIAssistViewComponent as EjsAiassistview } from "@syncfusion/ej2-vue-in
 @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-notifications/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-interactive-chat/styles/material.css";
 </style>
