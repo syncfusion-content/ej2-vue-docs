27ba4d2a32d24d08d1249b689d88a91931e336de
Build Automaion <buildautomation@syncfusion.com>
2024-10-09 04:58:55 +0530

    Integrated latest changes at 10-09-2024 4:30:09 AM

---
+++ ej2-vue/code-snippet/grid/state-management/default-cs5/app-composition.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <ejs-grid ref="grid" id="OrderDetails" :dataSource='data' :allowSorting='true' :enablePersistence='true'
+            :allowPaging='true' :allowFiltering='true' :actionBegin='actionBegin'>
+            <e-columns>
+                <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=120></e-column>
+                <e-column field='CustomerID' headerText='Customer ID' width=150></e-column>
+                <e-column field='ShipCity' headerText='Ship City' width=150></e-column>
+                <e-column field='ShipName' headerText='Ship Name' width=150></e-column>
+            </e-columns>
+        </ejs-grid>
+    </div>
+</template>
+<script setup>
+import { provide, ref } from "vue";
+import { GridComponent as EjsGrid, ColumnDirective as EColumn, ColumnsDirective as EColumns, Sort, Page, Filter } from "@syncfusion/ej2-vue-grids";
+import { data } from './datasource.js';
+const grid = ref(null);
+const actionBegin = function () {
+    grid.value.ej2Instances.query.addParams('dataSource', 'data');
+}
+provide('grid', [Sort, Page, Filter]);
+</script>
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-calendars/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-dropdowns/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-navigations/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-popups/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-splitbuttons/styles/tailwind.css";
+@import "../node_modules/@syncfusion/ej2-vue-grids/styles/tailwind.css";
+</style>
