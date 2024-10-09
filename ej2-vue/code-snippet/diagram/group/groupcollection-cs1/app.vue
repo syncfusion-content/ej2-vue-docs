4f9c5b59fb1667e038c0712bcbe9fa0de8c4cbac
Build Automaion <buildautomation@syncfusion.com>
2024-10-10 05:24:28 +0530

    Integrated latest changes at 10-10-2024 4:38:15 AM

--- ej2-vue/code-snippet/diagram/group/groupcollection-cs1/app.vue
+++ ej2-vue/code-snippet/diagram/group/groupcollection-cs1/app.vue
@@ -1,18 +1,17 @@
 <template>
     <div id="app">
-        <ejs-diagram id="diagram" ref="diagram" :width='width' :height='height' :nodes='nodes'
-            :getNodeDefaults='getNodeDefaults'></ejs-diagram>
+        <ejs-diagram id="diagram" ref="diagram" :width='width' :height='height' ></ejs-diagram>
     </div>
 </template>
 <script>
 import { DiagramComponent } from '@syncfusion/ej2-vue-diagrams';
-const group = [
+let group = [
     { id: "rectangle1", offsetX: 100, offsetY: 100, width: 100, height: 100, annotations: [{ content: 'node1' }] },
     { id: "rectangle2", offsetX: 200, offsetY: 200, width: 100, height: 100, annotations: [{ content: 'node2' }] },
     { id: 'group', children: ['rectangle1', 'rectangle2'] },
     { id: "rectangle3", offsetX: 400, offsetY: 400, width: 100, height: 100, annotations: [{ content: 'node1' }] },
     { id: "rectangle4", offsetX: 500, offsetY: 500, width: 100, height: 100, annotations: [{ content: 'node2' }] },
-    { id: 'group', children: ['rectangle3', 'rectangle4'], padding: { left: 10, right: 10, top: 10, bottom: 10 } },
+    { id: 'group2', children: ['rectangle3', 'rectangle4'], padding: { left: 10, right: 10, top: 10, bottom: 10 } },
 ];
 export default {
     name: "App",
@@ -23,14 +22,6 @@ export default {
         return {
             width: "100%",
             height: "350px",
-            nodes: nodes,
-            getNodeDefaults: (node) => {
-                node.height = 100;
-                node.width = 100;
-                node.style.fill = '#6BA5D7';
-                node.style.strokeColor = 'white';
-                return node;
-            },
         }
     },
     mounted: function () {
--- ej2-vue/code-snippet/diagram/group/groupcollection-cs1/index.js
+++ ej2-vue/code-snippet/diagram/group/groupcollection-cs1/index.js
@@ -1,37 +1,29 @@
 import Vue from 'vue';
 import { DiagramPlugin,NodeModel } from '@syncfusion/ej2-vue-diagrams';
 Vue.use(DiagramPlugin);
-const group:NodeModel = [
+let group:NodeModel = [
     { id: "rectangle1", offsetX: 100, offsetY: 100, width: 100, height: 100, annotations: [{ content: 'node1' }] },
     { id: "rectangle2", offsetX: 200, offsetY: 200, width: 100, height: 100, annotations: [{ content: 'node2' }] },
     { id: 'group', children: ['rectangle1', 'rectangle2'] },
     { id: "rectangle3", offsetX: 400, offsetY: 400, width: 100, height: 100, annotations: [{ content: 'node1' }] },
     { id: "rectangle4", offsetX: 500, offsetY: 500, width: 100, height: 100, annotations: [{ content: 'node2' }] },
-    { id: 'group', children: ['rectangle3', 'rectangle4'], padding: { left: 10, right: 10, top: 10, bottom: 10 } },
+    { id: 'group2', children: ['rectangle3', 'rectangle4'], padding: { left: 10, right: 10, top: 10, bottom: 10 } },
 ];
 new Vue({
 	el: '#app',
 	template: `
     <div id="app">
-        <ejs-diagram id="diagram"  :width='width' :height='height' :nodes='nodes' :getNodeDefaults='getNodeDefaults'></ejs-diagram>
+        <ejs-diagram id="diagram"  :width='width' :height='height' ></ejs-diagram>
     </div>
 `,
 
-    name: 'app'
+    name: 'app',
     data() {
         return {
             width: "100%",
             height: "350px",
-            nodes: nodes,
-            getNodeDefaults: (node) => {
-                node.height = 100;
-                node.width = 100;
-                node.style.fill = '#6BA5D7';
-                node.style.strokeColor = 'white';
-                return node;
-            },
         }
-    }
+    },
     mounted: function() {
         let diagramInstance: Diagram;
         let diagramObj: any = document.getElementById("diagram");
