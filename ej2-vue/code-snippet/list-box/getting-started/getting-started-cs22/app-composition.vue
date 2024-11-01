a77bfc76f59baee1126badd4e224e0df15755bbe
Build Automaion <buildautomation@syncfusion.com>
2024-11-02 04:55:14 +0530

    Integrated latest changes at 11-02-2024 4:30:50 AM

---
+++ ej2-vue/code-snippet/list-box/getting-started/getting-started-cs22/app-composition.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div id="container" style="margin:10px auto 0; width:250px;">
+      <label>Enter Text: </label>
+      <input type="text" id="filterTextBox" @input="handleFilterChange" placeholder="Enter text to filter"/>
+      <h4>Select your favorite car:</h4>
+      <ejs-listbox id="listbox" :dataSource="data"></ejs-listbox>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ListBoxComponent as EjsListbox } from "@syncfusion/ej2-vue-dropdowns";
+
+const data = [
+  { text: 'Hennessey Venom', id: 'list-01' },
+  { text: 'Bugatti Chiron', id: 'list-02' },
+  { text: 'Bugatti Veyron Super Sport', id: 'list-03' },
+  { text: 'SSC Ultimate Aero', id: 'list-04' },
+  { text: 'Koenigsegg CCR', id: 'list-05' },
+  { text: 'McLaren F1', id: 'list-06' },
+  { text: 'Aston Martin One- 77', id: 'list-07' },
+  { text: 'Jaguar XJ220', id: 'list-08' },
+  { text: 'McLaren P1', id: 'list-09' },
+  { text: 'Ferrari LaFerrari', id: 'list-10' },
+];
+const handleFilterChange = function (event) {
+  let listboxobj = document.getElementById('listbox').ej2_instances[0];
+  if (listboxobj) {
+    listboxobj.filter(data, new Query().where('text', 'contains', event.target.value, true));
+  }
+}
+</script>
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-vue-dropdowns/styles/material.css";
+</style>
---
+++ ej2-vue/list-box/how-to/filter-listbox.md
@@ -0,0 +1,24 @@
+---
+layout: post
+title:  Filter ListBox Data Using TextBox Component | Syncfusion
+description: Learn here all about Filter ListBox Data Using TextBox Component of Syncfusion Essential JS 2 and more.
+control: Filter ListBox
+platform: ej2-vue
+documentation: ug
+domainurl: ##DomainURL##
+---
+
+# How to Filter ListBox Data Using TextBox Component
+
+This example demonstrates how to filter Syncfusion ListBox data based on input from a TextBox. Bind an input event listener to the TextBox to capture user input and filter the items in the ListBox. Within the event handler, use the [`filter`](https://ej2.syncfusion.com/vue/documentation/api/list-box/#filter) method to update the ListBox items, ensuring that only those matching the input text are included.
+
+{% tabs %}
+{% highlight html tabtitle="Composition API (~/src/App.vue)" %}
+{% include code-snippet/list-box/getting-started/getting-started-cs22/app-composition.vue %}
+{% endhighlight %}
+{% highlight html tabtitle="Options API (~/src/App.vue)" %}
+{% include code-snippet/list-box/getting-started/getting-started-cs22/app.vue %}
+{% endhighlight %}
+{% endtabs %}
+        
+{% previewsample "page.domainurl/code-snippet/list-box/getting-started/getting-started-cs22" %}
