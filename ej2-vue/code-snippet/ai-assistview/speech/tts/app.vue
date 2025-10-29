31c5de622220a5da61a88bd87c7b82b615c3c401
Build Automaion <buildautomation@syncfusion.com>
2025-10-30 05:06:02 +0530

    Integrated latest changes at 10-30-2025 4:30:10 AM

--- ej2-vue/code-snippet/ai-assistview/speech/tts/app.vue
+++ ej2-vue/code-snippet/ai-assistview/speech/tts/app.vue
@@ -158,6 +158,8 @@ export default {
 
 @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-notifications/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-interactive-chat/styles/material.css";
