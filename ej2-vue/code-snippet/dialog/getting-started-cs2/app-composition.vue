a667778a9b5fcd7f381cdf0a7c8c6a0612e0d4ca
Build Automaion <buildautomation@syncfusion.com>
2025-11-06 07:40:32 +0530

    Integrated latest changes at 11-06-2025 7:30:04 AM

---
+++ ej2-vue/code-snippet/dialog/getting-started-cs2/app-composition.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div id="target" class="control-section">
+      <ejs-dialog :target="data[0].target" :width="data[0].width" :content="data[0].content">
+      </ejs-dialog>
+    </div>
+  </div>
+</template>
+<script setup>
+import { DialogComponent as EjsDialog } from "@syncfusion/ej2-vue-popups";
+  const data = [{ target: "#target",
+                  width: "335px",
+                  content: "This is a Dialog with content.",}]
+</script>
+<style>
+  .control-section {
+    height: 100%;
+    min-height: 200px;
+  }
+</style>
