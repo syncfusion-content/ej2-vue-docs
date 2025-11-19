f661c37f47ad58740687a81ed544c68b54a39da4
Build Automaion <buildautomation@syncfusion.com>
2025-11-20 04:48:28 +0530

    Integrated latest changes at 11-20-2025 4:30:06 AM

---
+++ ej2-vue/code-snippet/treegrid/selection/default-cs7/app-composition.vue
@@ -0,0 +1,32 @@
+<template>
+<div id="app">
+        <ejs-treegrid :dataSource='data' idMapping='TaskID' parentIdMapping='ParentID' :treeColumnIndex='1' :enableVirtualization="true"  :selectionSettings='selectionOptions' :isRowSelectable="isRowSelectable" >
+            <e-columns>
+                <e-column type="checkbox" width=90></e-column>
+                        <e-column field='Task'  headerText='Task'  width=300></e-column>
+                        <e-column field='TaskID' isPrimaryKey="true" visible="false"></e-column>
+                        <e-column field='AssignedTo'  headerText='Assigned To'  width=140></e-column>
+                        <e-column field='StartDate' headerText='Start' format='yMd' width=180></e-column>
+                        <e-column field='DueDate' headerText='Due' format='yMd' width=180></e-column>
+                        <e-column field='Priority' headerText='Priority' width=90></e-column>
+                        <e-column field='Progress' headerText='Progress' width=110></e-column>
+            </e-columns>
+        </ejs-treegrid>
+</div>
+</template>
+<script setup>
+import { provide } from "vue";
+
+import { TreeGridComponent as EjsTreegrid, VirtualScroll, ColumnDirective as EColumn, ColumnsDirective as EColumns } from "@syncfusion/ej2-vue-treegrid";
+import { sampleData } from "./datasource.js";
+
+const data = sampleData;
+
+const selectionOptions= { persistSelection:true };
+const isRowSelectable = function(data, columns) {
+   return data.Progress !== 'Completed';
+};
+provide('treegrid',  [ VirtualScroll ]);
+
+
+</script>
