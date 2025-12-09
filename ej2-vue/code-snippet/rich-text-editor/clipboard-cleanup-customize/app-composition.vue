f446a8d0913849825c15e8da2f2b6685bfb410f0
Build Automaion <buildautomation@syncfusion.com>
2025-12-09 07:42:00 +0530

    Integrated latest changes at 12-09-2025 7:30:04 AM

---
+++ ej2-vue/code-snippet/rich-text-editor/clipboard-cleanup-customize/app-composition.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <ejs-richtexteditor ref="rteObj" :beforeClipboardWrite="beforeClipboardWrite"></ejs-richtexteditor>  
+    </div>
+</div>
+</template>
+
+<script setup>
+import { provide } from 'vue';
+import { RichTextEditorComponent as EjsRichtexteditor, Toolbar, HtmlEditor, ClipboardCleanup } from "@syncfusion/ej2-vue-richtexteditor";
+import { detach } from '@syncfusion/ej2-base';
+
+const beforeClipboardWrite = (args) => {
+    if (args.operation === 'copy') {
+        args.htmlContent = '<h1>This is customized content triggered while copy event</h1>';
+        args.plainTextContent = 'This is customized content triggered while copy event';
+    }
+},
+provide('richtexteditor', [Toolbar, HtmlEditor, ClipboardCleanup]);
+</script>
+
+<style>
+@import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-lists/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-vue-richtexteditor/styles/material.css";
+</style>
---
+++ ej2-vue/code-snippet/rich-text-editor/clipboard-cleanup/app-composition.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ejs-richtexteditor></ejs-richtexteditor>  
+    </div>
+</div>
+</template>
+
+<script setup>
+import { provide } from 'vue';
+import { RichTextEditorComponent as EjsRichtexteditor, Toolbar, HtmlEditor, ClipboardCleanup } from "@syncfusion/ej2-vue-richtexteditor";
+
+provide('richtexteditor', [Toolbar, HtmlEditor, ClipboardCleanup]);
+</script>
+
+<style>
+@import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-lists/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-vue-richtexteditor/styles/material.css";
+</style>
