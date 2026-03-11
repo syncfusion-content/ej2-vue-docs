9b617595bcbc88ece1184acee5f84eb8099deb8e
KarthikSelviPonnusamy <karthikselvi.ponnusamy@syncfusion.com>
2026-03-11 19:25:17 +0530

    1015214: Resolved confilicts

--- ej2-vue/code-snippet/rich-text-editor/toolbar-cs22/app-composition.vue
+++ ej2-vue/code-snippet/rich-text-editor/toolbar-cs22/app-composition.vue
@@ -1,23 +1,6 @@
 <template>
   <div>
-    <ejs-richtexteditor ref="rteObj" :toolbarSettings="toolbarSettings">
-      <p>The rich text editor component is WYSIWYG ("what you see is what you get") editor that provides the best user
-        experience to create and update the content. Users can format their content using standard toolbar commands.</p>
-      <p><b>Key features:</b></p>
-      <ul>
-        <li>
-          <p>Provides &lt;IFRAME&gt; and &lt;DIV&gt; modes</p>
-        </li>
-        <li>
-          <p>Capable of handling markdown editing.</p>
-        </li>
-        <li>
-          <p>Contains a modular library to load the necessary functionality on demand.</p>
-        </li>
-        <li>
-          <p>Provides a fully customizable toolbar.</p>
-        </li>
-      </ul>
+    <ejs-richtexteditor ref="rteObj" :toolbarSettings="toolbarSettings" :value= "value">
     </ejs-richtexteditor>
   </div>
 </template>
@@ -33,6 +16,17 @@ const toolbarSettings = {
     'CreateLink', 'Image', '|', 'ClearFormat', 'Print',
     'SourceCode', 'FullScreen', '|', 'Undo', 'Redo']
 };
+const value = `
+  <p>The rich text editor component is WYSIWYG ("what you see is what you get") editor that provides the best user
+  experience to create and update the content. Users can format their content using standard toolbar commands.</p>
+  <p><b>Key features:</b></p>
+  <ul>
+    <li><p>Provides &lt;IFRAME&gt; and &lt;DIV&gt; modes</p></li>
+    <li><p>Capable of handling markdown editing.</p></li>
+    <li><p>Contains a modular library to load the necessary functionality on demand.</p></li>
+    <li><p>Provides a fully customizable toolbar.</p></li>
+  </ul>
+`;
 provide('richtexteditor', [Toolbar, Link, Count, Image, HtmlEditor, QuickToolbar]);
 </script>
 <style>
